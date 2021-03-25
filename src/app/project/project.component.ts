c1e6fbe922d6e11659bcab31012d465dd8260d87
ifotn <rich@infrontofthenet.com>
2021-03-25 14:40:33 -0400

    l9/2 - angular w/2 way mock data binding

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -7,6 +7,14 @@ export class Project {
   course: string;
 }
 
+// create mock list of projects
+const PROJECTS: Project[] = [
+  { id: 101, name: 'Lab 2', course: 'COMP2106' },
+  { id: 102, name: 'Quiz 3', course: 'COMP2106' },
+  { id: 103, name: 'Tutorial', course: 'COMP2106' },
+  { id: 104, name: 'Assignment 2', course: 'COMP2106' }
+]
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -14,10 +22,17 @@ export class Project {
 })
 export class ProjectComponent implements OnInit {
   // create an in-memory project for display
-  project: Project = {
+  /*project: Project = {
     id: 1,
     name: 'Lab 2',
     course: 'COMP2106'
+  }*/
+  // make mock data available to html view
+  projects = PROJECTS;
+  selectedProject: Project; // used to show a project in the form
+  // select the project the user clicked on
+  onSelect(project: Project): void {
+    this.selectedProject = project;
   }
   constructor() { }
 
